7789b3461b665de522e02693736a29a4108ef76b
User-CruZer <ahmadzakialbadri@gmail.com>
2025-02-03 13:35:16 +0700

    updated starter

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,28 @@
-let js = "amazing";
-if (js === "amazing") alert ("JavaScript is FUN");
+// let js = "amazing";
+// if (js === "amazing") alert ("JavaScript is FUN");
 
 console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+
+// part of learning variables on JavaScript
+let firstName = "jonas";
+// camelCase:
+let first
+let firstNamePerson
+// Others:
+let first_name_person
+// constant (a variable that cannot be changed)
+let PI = 3.1415;
+
+let myFirstJob = "Unemployed XD";
+let myCurrentJob = "College Student";
+
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(myFirstJob);
