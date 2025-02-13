6fa1b8b4f746bbba9310a8d8896b07fff5ddacac
User-CruZer <ahmadzakialbadri@gmail.com>
2025-02-13 17:53:27 +0700

    Function decleration and expression in javascript

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,12 @@
-// using strict mode:\
-/*
+// using strict mode:
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("You can drive")
-*/
 
 // undestanding function:
 
@@ -19,3 +18,21 @@ function joko(operator, keys) {
 }
 
 joko()
+*/
+
+// function declaration vs expression
+
+
+// function decleration
+function calcAge1(birthyear) {
+    return 2025 - birthyear;
+}
+const age1 = calcAge1(2006);
+
+// function expression
+const calcAge2 = function(birthyear) {
+    return 2025 - birthyear
+}
+const age2 = calcAge2(2008);
+console.log(age1, age2);
+
