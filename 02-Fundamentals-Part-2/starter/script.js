10fe8bd340c6df62c5bbc42ace8574bd68cc1b83
User-CruZer <ahmadzakialbadri@gmail.com>
2025-02-13 14:27:09 +0700

    some scratch and strict mode in javascript

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,8 @@
+// using strict mode:
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("You can drive")
