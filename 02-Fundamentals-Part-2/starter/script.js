291d54f6f5baa0f120e61ff4da244e7f37fbf994
User-CruZer <ahmadzakialbadri@gmail.com>
2025-02-13 14:35:37 +0700

    understanding javascript function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,5 @@
-// using strict mode:
+// using strict mode:\
+/*
 "use strict";
 
 let hasDriversLicense = false;
@@ -6,3 +7,15 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("You can drive")
+*/
+
+// undestanding function:
+
+function joko(operator, keys) {
+    operator = prompt("whats your name?");
+    console.log(`hello ${operator}`);
+    keys = prompt("what's your key number?");
+    console.log(`so ${keys} is your key number!`)
+}
+
+joko()
