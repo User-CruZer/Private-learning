da568995a13003f2c27cedf1d128ce4ccdd5f5d6
User-CruZer <ahmadzakialbadri@gmail.com>
2025-02-19 14:06:19 +0700

    coding exercise 6

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -197,3 +197,16 @@ if (friends.includes("Ridwan")) {
     console.log("you dont have friend called Ridwan")
 }
 */
+
+// Coding exercise 6:
+
+function calcTip(bill) {
+    bill = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+    return bill
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [(tips[0] + bills[0]), (bills[1] + tips[1]), (bills[2], + tips[2])];
+
+console.log(totals);
